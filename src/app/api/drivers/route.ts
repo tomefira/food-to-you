986f81a8e21f94ecb68eb7fe84f23386178b6b1c
tomefira <xtomazinnn@gmail.com>
2024-05-07 11:20:00 +1000

    Further to debug route issue

--- src/app/api/drivers/route.ts
+++ src/app/api/drivers/route.ts
@@ -1,5 +1,7 @@
+import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
-import prisma from '../../../lib/prisma';
+
+const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
   const data = await request.json();
